bb9f8129b7957213e6bfc869a48221f816bf018e
mishalajmeri <162704831+mishalajmeri@users.noreply.github.com>
2024-04-01 00:08:55 +0500

    push my cli_number_guessing_project

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-6",
+    },
+]);
+if (answer.userguessednumber === randomNumber) {
+    console.log("congratulation! your answer is correct");
+}
+else {
+    console.log("your answer is wrong");
+}
